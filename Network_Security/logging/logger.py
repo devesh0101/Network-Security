d6b945edc107b95e8d0fa7f64df396ac84cb432c
Devesh Dhyani <devesh@Deveshs-MacBook-Air.local>
2025-12-03 15:15:47 +0530

    Exception handling and logging

---
+++ Network_Security/logging/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+import datetime
+
+LOG_FILE = f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
