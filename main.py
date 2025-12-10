d222a88c9c46766f1cd0721c550f39b226b23a93
Devesh Dhyani <devesh@Deveshs-MacBook-Air.local>
2025-12-10 11:08:16 +0530

    Model Trainer

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from Network_Security.logging.logger import logging
 from Network_Security.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 import sys
 
+
+from Network_Security.components.model_trainer import ModelTrainer
+from Network_Security.entity.config_entity import ModelTrainerConfig
+
 if __name__=='__main__':
     try:
         trainingPipelineConfig=TrainingPipelineConfig()
@@ -27,6 +31,13 @@ if __name__=='__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Data Transformation completed")  
         print(data_transformation_artifact)
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingPipelineConfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
     
 
